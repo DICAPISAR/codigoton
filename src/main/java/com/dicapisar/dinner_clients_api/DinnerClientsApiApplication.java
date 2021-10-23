c8fb85f14735934e0612da71877c1c8d421cf92d
Diego Camilo Pinto Sarmiento <dicapisar@gmail.com>
2021-10-22 23:02:30 -0500

    first commit

---
+++ src/main/java/com/dicapisar/dinner_clients_api/DinnerClientsApiApplication.java
@@ -0,0 +1,13 @@
+package com.dicapisar.dinner_clients_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinnerClientsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinnerClientsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dicapisar/dinner_clients_api/DinnerClientsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dicapisar.dinner_clients_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinnerClientsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
